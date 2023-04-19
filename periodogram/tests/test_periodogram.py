35917b95c0071e7132787cc72d605ec02c36ab49
Daniel Hey <danielhey@outlook.com>
2023-04-19 13:41:35 -1000

    clean up tests

--- periodogram/tests/test_periodogram.py
+++ periodogram/tests/test_periodogram.py
@@ -17,27 +17,8 @@ def test_amplitude_normalization():
 
 
 def test_psd_normalization():
-    tmax = x.max()
-    tmin = x.min()
-    df = 1.0 / (tmax - tmin)
-    fmin = df
-    fmax = 0.5 / np.median(np.diff(x))  # *nyq_mult
-
-    N = len(x)
-
-    freq_window = np.arange(fmin, fmax, df / 1)
-    nu = 0.5 * (fmin + fmax)
-    power_window = (
-        LombScargle(x, np.sin(2 * np.pi * nu * x)).power(
-            freq_window, normalization="psd"
-        )
-        / N
-        * 4.0
-    )
-    Tobs = 1.0 / np.sum(np.median(freq_window[1:] - freq_window[:-1]) * power_window)
-
-    freq, p = psd(x, y, fmin=fmin, fmax=fmax)
-    assert np.isclose(p.max(), amplitude**2 * Tobs, rtol=1e-3)
+    freq, p = psd(x, y)
+    assert np.isclose(p.max(), amplitude**2 * (x[-1] - x[0]), rtol=1e-3)
 
 
 def test_power_normalization():
